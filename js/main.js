95b786f5ec933d882fab0cc2670c5e48285ffe9f
Jahongir <jahongir.bekmuhammetov@gmail.com>
2021-02-06 19:53:54 +0600

    fetch error

--- js/main.js
+++ js/main.js
@@ -10,39 +10,47 @@ function getParameterByName(name, url = window.location.href) {
 const mainUrl = "https://jahongir28.github.io/wedding?id=ccfc1369-3769-4db9-b325-ab950d677e60"
 
 $(document).ready(function () {
-    //const guests = JSON.parse(guests)
-    let queryId = getParameterByName('id').replace(/"/g, '');
+    let queryId = getParameterByName('id')
+    if (queryId == null) {
+        $( ".content-main" ).hide();
+        $("#error-message").text('Неверная ссылка')
+        $( ".error-message" ).show();
+        return
+    }
+    let clearId = queryId.replace(/"/g, '');
     console.log("queryId:" + queryId)
     $.getJSON("guests.json", function (result) {
         console.log("success " + result);
-        var names = []
-        $.each(result, function(i, field){
-            if (queryId === field.id){
+        let names = []
+        let gender = ""
+        $.each(result, function (i, field) {
+            if (clearId === field.id) {
                 names = field.names
-                if (names.length === 2){
-                    let name1 = names[0] + " и"
-                    $("#guest-name-1").text(name1)
-                    $("#guest-name-2").text(names[1])
-                }else {
-                    $("#guest-name-1").text(names)
-                }
-                console.log("FOUND")
+                gender = field.gender
             }
-            console.log(field.id)
-            console.log(field.names)
-            console.log("############################################")
         });
-        console.log("names:" + names)
-        /*var names = []
-        for (let i = 0; i < result.length; i++) {
-            let fileId = result[i].id
-            if (fileId === queryId){
-                names = result[i].names
-                break
-            }
-            console.log(fileId)
-        }*/
+        if (names.length === 0){
+            $( ".content-main" ).hide();
+            $("#error-message").text('Пригласительный не найден')
+            $( ".error-message" ).show();
+            return
+        }
 
+        if (names.length === 2) {
+            $("#welcome-title").text('Уважаемые')
+            let name1 = names[0] + " и"
+            $("#guest-name-1").text(name1)
+            $("#guest-name-2").text(names[1])
+        } else {
+            let prefix
+            if (gender === "F"){
+                prefix = 'Уважаемая'
+            }else {
+                prefix = 'Уважаемый'
+            }
+            $("#welcome-title").text(prefix)
+            $("#guest-name-1").text(names)
+        }
     })
 });
 
